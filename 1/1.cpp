438f9cb9f2a0ba934aa08cf47d4b985332246c02
ebonitesh0ut <ebonitesh0ut@DESKTOP-OHTQSP9>
2021-11-10 00:03:08 +0300

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stdarg.h>
+#include <string>
+#include <list>
+
+
+
+
+bool searchFunc(std::string& substr, std::string& sourceString) {
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+	return 0;
+}
